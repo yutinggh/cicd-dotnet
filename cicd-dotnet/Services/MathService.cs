ee457c500977b56158a6255063aa5f3e8824a4b2
Yuting Huang <huang.yu.tingting@gmail.com>
2024-07-03 13:26:32 +0800

    fix: dotnet format

--- cicd-dotnet/Services/MathService.cs
+++ cicd-dotnet/Services/MathService.cs
@@ -2,7 +2,7 @@ namespace cicd_dotnet.Services
 {
     public class MathService
     {
-        public int Add(int a, int b) => a + b; 
+        public int Add(int a, int b) => a + b;
         public int Subtract(int a, int b) => a - b;
         public int Multiply(int a, int b) => a * b;
         public int Divide(int a, int b) => a / b;
