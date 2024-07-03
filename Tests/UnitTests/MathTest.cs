81f628394aac46c49f6026317a82cba7b3979472
Yuting Huang <huang.yu.tingting@gmail.com>
2024-07-03 17:40:14 +0800

    test: both unit and regression test report combined

--- Tests/UnitTests/MathTest.cs
+++ Tests/UnitTests/MathTest.cs
@@ -6,7 +6,7 @@ namespace UnitTests
     public class MathTests
     {
         [Fact]
-        public void Add_TwoNumbers_ReturnsSum()
+        public void Add_TwoNumbers()
         {
             // Arrange
             var mathService = new MathService();
@@ -21,6 +21,21 @@ namespace UnitTests
         }
 
         [Fact]
+        public void Subtract_TwoNumbers()
+        {
+            // Arrange
+            var mathService = new MathService();
+            var a = 5;
+            var b = 3;
+
+            // Act
+            var result = mathService.Subtract(a, b);
+
+            // Assert
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
         public void Multiply_TwoNumbers()
         {
             var mathService = new MathService();
@@ -29,6 +44,21 @@ namespace UnitTests
             var result = mathService.Multiply(a, b);
 
             Assert.Equal(15, result);
+        }
+
+        [Fact]
+        public void Divide_TwoNumbers()
+        {
+            // Arrange
+            var mathService = new MathService();
+            var a = 6;
+            var b = 3;
+
+            // Act
+            var result = mathService.Divide(a, b);
+
+            // Assert
+            Assert.Equal(2, result);
         }
     }
 }
