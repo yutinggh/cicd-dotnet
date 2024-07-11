bc6c50e6b7bf934364f8eea13c02b3aa31d69d54
Yuting Huang <huang.yu.tingting@gmail.com>
2024-07-11 17:53:17 +0800

    feat: edit road to integration and release to be jobs

--- cicd-dotnet/Services/MathService.cs
+++ cicd-dotnet/Services/MathService.cs
@@ -4,7 +4,7 @@ namespace cicd_dotnet.Services
     {
         public int Add(int a, int b) => a + b;
         public int Subtract(int a, int b) => a - b;
-        public int Multiply(int a, int b) => a * b;
-        public int Divide(int a, int b) => a / b;
+        // public int Multiply(int a, int b) => a * b;
+        // public int Divide(int a, int b) => a / b;
     }
 }
